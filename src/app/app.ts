cd9c73774c83ea9a9f538c93a3cab85c2a743a5f
jainras-peter <jainras2004@gmail.com>
2026-01-11 20:25:18 +0530

    setup primeng and tailwindcss

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrimengTesting } from './primeng-testing/primeng-testing';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,PrimengTesting],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/primeng-testing/primeng-testing.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'primeng-testing',
+    templateUrl: './primeng-testing.html',
+    standalone: true,
+    imports: [ButtonModule]
+})
+export class PrimengTesting {
+
+
+}
